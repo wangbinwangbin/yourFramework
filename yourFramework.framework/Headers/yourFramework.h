71545f1f85b84c7dc3328a85d972a5785b45728c
wangbin <bin.wang@mtime.com>
2017-01-06 13:35:07 +0800

    framework

---
+++ yourFramework.framework/Headers/yourFramework.h
@@ -0,0 +1,19 @@
+//
+//  yourFramework.h
+//  yourFramework
+//
+//  Created by wangbin on 2017/1/6.
+//  Copyright © 2017年 wangbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for yourFramework.
+FOUNDATION_EXPORT double yourFrameworkVersionNumber;
+
+//! Project version string for yourFramework.
+FOUNDATION_EXPORT const unsigned char yourFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <yourFramework/PublicHeader.h>
+
+
---
+++ yourFramework/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  yourFramework
+//
+//  Created by wangbin on 2017/1/6.
+//  Copyright © 2017年 wangbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (void)onClick;
+@end
